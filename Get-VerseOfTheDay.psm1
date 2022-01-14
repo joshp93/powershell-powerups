25b188a2edb6f10d61c9fb8ff2dc9b3e5066ae72
Joshua Pearson <joshua.pearson@nationalexpress.com>
2022-01-14 11:44:54 +0000

    Updated to verse of the day

--- Get-VerseOfTheDay.psm1
+++ Get-VerseOfTheDay.psm1
@@ -1,7 +1,11 @@
 ﻿function Get-VerseOfTheDay {
     $ApiUrl = "https://labs.bible.org/api/?passage=votd&formatting=plain"
-    $ApiAnswer = Invoke-WebRequest $ApiUrl
-    return $ApiAnswer
+    try {
+        $ApiAnswer = Invoke-WebRequest $ApiUrl
+        return $ApiAnswer
+    } catch {
+        
+    }
 }
 
 Export-ModuleMember -Function Get-VerseOfTheDay
