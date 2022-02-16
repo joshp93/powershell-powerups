e73d6ddacd1b6fe5d81906072ef5af367c74e800
Joshua Pearson <joshua.pearson@nationalexpress.com>
2022-02-16 14:18:24 +0000

    🥓

--- Git-AddCommitWithCardIdAndMessage.psm1
+++ Git-AddCommitWithCardIdAndMessage.psm1
@@ -1,13 +1,33 @@
 function Git-AddCommitWithCardIdAndMessage {
     Param(
-     [parameter(Mandatory=$true)]
-     [string] $Message
-     )
+        [parameter(Mandatory = $true)]
+        [string] $Message
+    )
     $IsGitRepo = Check-LocationIsGitRepo
     if ($IsGitRepo) {
         $BranchName = Get-CurrentBranchName
         if ($BranchName -eq "master") {
-            Throw "You are on master you berk"
+            Write-Host "You are on the master you berk" -ForegroundColor Red
+            Write-Host "Let's create a new branch together..." -ForegroundColor DarkGreen
+            $BranchType = Read-Host "Is it a feature or a bug? [f] feature | [b] bug | [default/other] something else" -ForegroundColor DarkGreen
+            if ($BranchType -eq "f" -or $BranchType -eq "b") {
+                $CardName = Read-Host "What's the card name?" -ForegroundColor DarkGreen
+                if ($BranchType -eq "f") {
+                    if (Check-BranchExists "feature/${CardName}") {
+                        Write-Host "That branch already exists, you'd better check it manually." -ForegroundColor Red
+                        return
+                    }
+                    cfb $CardName
+                } else {
+                    if (Check-BranchExists "fix/${CardName}") {
+                        Write-Host "That branch already exists, you'd better check it manually." -ForegroundColor Red
+                        return
+                    }
+                    cbb $CardName
+                }
+            } else {
+                throw "Whoops, try again"
+            }
         }
         $CardName = $BranchName.Split('/')[1].ToUpper()
         $Message = "${CardName} ${Message}"
--- Pull-NXRestAndMA.psm1
+++ Pull-NXRestAndMA.psm1
@@ -3,10 +3,12 @@
     if ($IsGitRepo) {
         Write-Host "Pulling latest nx-rest" -ForegroundColor DarkGreen
         cd C:\apps\nx-rest-api
+        git ch master
         git pull
 
         Write-Host "Pulling latest ma" -ForegroundColor DarkGreen
         cd C:\apps\nx-my-account-api
+        git ch master
         git pull
     } else {
         return
