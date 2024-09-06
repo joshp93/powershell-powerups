5cc23b9ab8ab1b65416bce6c75b0197e58a0be09
Josh Pearson <joshp@aferry.com>
2024-09-06 12:28:37 +0100

    Change the path to C:\powershell-powerups\

--- Load-Scripts.psm1
+++ Load-Scripts.psm1
@@ -1,22 +1,22 @@
 ﻿Import-Module posh-git
-Import-Module C:\PowerShell\Check-LocationIsGitRepo.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Get-CurrentBranchName.psm1
-Import-Module C:\PowerShell\Check-BranchExists.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Push-CurrentBranchUpstream.psm1
-Import-Module C:\PowerShell\Create-FeatureBranch.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Create-BugBranch.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Pull-AllGitRepos.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Run-NpmInstallForAll.psm1  -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Convert-JavaToTS.psm1  -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Merge-LatestRootIntoThisBranch.psm1  -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Pull-NXRestAndMA.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Get-VerseOfTheDay.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Git-AddCommitWithCardIdAndMessage.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Get-RootBranchName.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Git-CheckoutRoot.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Remove-OldLocalBranches.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Get-Settings.psm1 -WarningAction SilentlyContinue
-Import-Module C:\PowerShell\Set-Settings.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Check-LocationIsGitRepo.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Get-CurrentBranchName.psm1
+Import-Module C:\powershell-powerups\Check-BranchExists.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Push-CurrentBranchUpstream.psm1
+Import-Module C:\powershell-powerups\Create-FeatureBranch.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Create-BugBranch.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Pull-AllGitRepos.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Run-NpmInstallForAll.psm1  -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Convert-JavaToTS.psm1  -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Merge-LatestRootIntoThisBranch.psm1  -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Pull-NXRestAndMA.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Get-VerseOfTheDay.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Git-AddCommitWithCardIdAndMessage.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Get-RootBranchName.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Git-CheckoutRoot.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Remove-OldLocalBranches.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Get-Settings.psm1 -WarningAction SilentlyContinue
+Import-Module C:\powershell-powerups\Set-Settings.psm1 -WarningAction SilentlyContinue
 
 set-alias -Name cbn -Value Get-CurrentBranchName
 set-alias -Name pbu -Value Push-CurrentBranchUpstream
