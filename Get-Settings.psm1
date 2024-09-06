73c2ad8f06ae7ce8ab1edffb91469749d2c9c631
Josh Pearson <joshp@aferry.com>
2024-09-06 12:30:59 +0100

    Updates to the settings (catch errors and remove usage as it's not needed right now)

--- Get-Settings.psm1
+++ Get-Settings.psm1
@@ -1,5 +1,10 @@
 function Get-Settings {
-   return Get-Content 'C:\PowerShell\Settings.json' | Out-String | ConvertFrom-Json
+   try {
+      return Get-Content 'C:\powershell-powerups\Settings.json' | Out-String | ConvertFrom-Json
+   }
+   catch {
+      Write-Warning "Couldn't find settings file"
+   }
 }
 
 Export-ModuleMember -Function Get-Settings
