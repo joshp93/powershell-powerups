8a656b39cc943bb57629f1bf8d7f1f198c574bbc
Josh Pearson <joshp@aferry.com>
2024-09-23 08:24:50 +0100

    Factored aliases into their own module and used PSScriptRoot for path

--- Load-Scripts.psm1
+++ Load-Scripts.psm1
@@ -1,47 +1,20 @@
 ﻿Import-Module posh-git
-Import-Module C:\powershell-powerups\Check-LocationIsGitRepo.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Get-CurrentBranchName.psm1
-Import-Module C:\powershell-powerups\Check-BranchExists.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Push-CurrentBranchUpstream.psm1
-Import-Module C:\powershell-powerups\Create-FeatureBranch.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Create-BugBranch.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Pull-AllGitRepos.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Run-NpmInstallForAll.psm1  -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Convert-JavaToTS.psm1  -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Merge-LatestRootIntoThisBranch.psm1  -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Get-VerseOfTheDay.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Git-AddCommitWithCardIdAndMessage.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Get-RootBranchName.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Git-CheckoutRoot.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Remove-OldLocalBranches.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Get-Settings.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Set-Settings.psm1 -WarningAction SilentlyContinue
-Import-Module C:\powershell-powerups\Npm-RunScript.psm1 -WarningAction SilentlyContinue
-
-set-alias -Name cbn -Value Get-CurrentBranchName
-set-alias -Name pbu -Value Push-CurrentBranchUpstream
-set-alias -Name cfb -Value Create-FeatureBranch
-set-alias -Name cbb -Value Create-BugBranch
-set-alias -Name mlr -Value Merge-LatestRootIntoThisBranch
-set-alias -Name gacm -Value Git-AddCommitWithCardIdAndMessage
-Set-Alias -Name cm -Value Git-CheckoutRoot
-Set-Alias -Name nr -Value Npm-RunScript
-Set-Alias -Name sbc -Value Start-BuyerContentMainAndStartThis
-
-function Build-QwikClientServer { npm run build.client; npm run build.ssr }
-Set-Alias -Name qbcs -Value Build-QwikClientServer
-
-function Build-QwikClientOnly { npm run build.client }
-Set-Alias -Name qbc -Value Build-QwikClientOnly
-
-function Build-QwikServerOnly { npm run build.ssr }
-Set-Alias -Name qbs -Value Build-QwikServerOnly
-
-function Build-QwikBuildAndTest { npm run build.client; npm run build.ssr; npm test }
-Set-Alias -Name qbt -Value Build-QwikBuildAndTest
-
-function fmlrp { Merge-LatestRootIntoThisBranch $true }
-set-alias -Name mlrp -Value fmlrp
-
-function fcmp { Git-CheckoutRoot $true }
-Set-Alias -Name cmp -Value fcmp
+Import-Module $PSScriptRoot\Check-LocationIsGitRepo.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Get-CurrentBranchName.psm1
+Import-Module $PSScriptRoot\Check-BranchExists.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Push-CurrentBranchUpstream.psm1
+Import-Module $PSScriptRoot\Create-FeatureBranch.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Create-BugBranch.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Pull-AllGitRepos.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Run-NpmInstallForAll.psm1  -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Convert-JavaToTS.psm1  -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Merge-LatestRootIntoThisBranch.psm1  -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Get-VerseOfTheDay.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Git-AddCommitWithCardIdAndMessage.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Get-RootBranchName.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Git-CheckoutRoot.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Remove-OldLocalBranches.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Get-Settings.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Set-Settings.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Npm-RunScript.psm1 -WarningAction SilentlyContinue
+Import-Module $PSScriptRoot\Set-CustomAliases.psm1
---
+++ Set-CustomAliases.psm1
@@ -0,0 +1,27 @@
+set-alias -Name cbn -Value Get-CurrentBranchName
+set-alias -Name pbu -Value Push-CurrentBranchUpstream
+set-alias -Name cfb -Value Create-FeatureBranch
+set-alias -Name cbb -Value Create-BugBranch
+set-alias -Name mlr -Value Merge-LatestRootIntoThisBranch
+set-alias -Name gacm -Value Git-AddCommitWithCardIdAndMessage
+Set-Alias -Name cm -Value Git-CheckoutRoot
+Set-Alias -Name nr -Value Npm-RunScript
+Set-Alias -Name sbc -Value Start-BuyerContentMainAndStartThis
+
+function Build-QwikClientServer { pnpm run build.client; pnpm run build.ssr }
+Set-Alias -Name qbcs -Value Build-QwikClientServer
+
+function Build-QwikClientOnly { pnpm run build.client }
+Set-Alias -Name qbc -Value Build-QwikClientOnly
+
+function Build-QwikServerOnly { pnpm run build.ssr }
+Set-Alias -Name qbs -Value Build-QwikServerOnly
+
+function Build-QwikBuildAndTest { pnpm run build.client; pnpm run build.ssr; pnpm test }
+Set-Alias -Name qbt -Value Build-QwikBuildAndTest
+
+function fmlrp { Merge-LatestRootIntoThisBranch $true }
+set-alias -Name mlrp -Value fmlrp
+
+function fcmp { Git-CheckoutRoot $true }
+Set-Alias -Name cmp -Value fcmp
