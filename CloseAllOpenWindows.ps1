df77e4790895a613f65708873464d1ab4160adf0
Josh Pearson <joshp@aferry.com>
2026-02-12 15:42:11 +0000

    feat: Update the CloseAllOpenWindows.ps1 script to work with WindowsTerminal

--- CloseAllOpenWindows.ps1
+++ CloseAllOpenWindows.ps1
@@ -4,13 +4,16 @@
 catch {
     Write-Warning "nircmd failed. You probably don't have it installed"
 }
-Get-Process | ? { $_.MainWindowTitle -ne "" -and ($_.ProcessName -ne "powershell" -and $_.ProcessName -ne "powershell_ise" -and $_.ProcessName -ne "WindowsInternal.ComposableShell.Experiences.TextInput.InputApp") } | Foreach-Object { $_.CloseMainWindow() | Out-Null }
-Get-Process | ? { $_.MainWindowTitle -ne "" -and ($_.ProcessName -ne "powershell" -and $_.ProcessName -ne "powershell_ise" -and $_.ProcessName -ne "WindowsInternal.ComposableShell.Experiences.TextInput.InputApp") } | Foreach-Object { $_.CloseMainWindow() | Out-Null }
+Get-Process | Where-Object { $_.MainWindowTitle -ne "" -and ($_.ProcessName -ne "WindowsTerminal" -and $_.ProcessName -ne "powershell_ise" -and $_.ProcessName -ne "WindowsInternal.ComposableShell.Experiences.TextInput.InputApp") } | Foreach-Object { $_.CloseMainWindow() | Out-Null }
+Get-Process | Where-Object { $_.MainWindowTitle -ne "" -and ($_.ProcessName -ne "WindowsTerminal" -and $_.ProcessName -ne "powershell_ise" -and $_.ProcessName -ne "WindowsInternal.ComposableShell.Experiences.TextInput.InputApp") } | Foreach-Object { $_.CloseMainWindow() | Out-Null }
+Get-Process | Where-Object { $_.MainWindowTitle -ne "" -and ($_.ProcessName -ne "WindowsTerminal" -and $_.ProcessName -ne "powershell_ise" -and $_.ProcessName -ne "WindowsInternal.ComposableShell.Experiences.TextInput.InputApp") } | Foreach-Object { $_.CloseMainWindow() | Out-Null }
+Get-Process | Where-Object { $_.MainWindowTitle -ne "" -and ($_.ProcessName -ne "WindowsTerminal" -and $_.ProcessName -ne "powershell_ise" -and $_.ProcessName -ne "WindowsInternal.ComposableShell.Experiences.TextInput.InputApp") } | Foreach-Object { $_.CloseMainWindow() | Out-Null }
 
 $action = Read-Host "What do you want to do? Shutdown (s) | Restart (r) | Nothing (n - default)"
 
 if ($action -eq "s") {
     shutdown /p
-} elseif ($action -eq "r") {
+}
+elseif ($action -eq "r") {
     Restart-Computer
 }
